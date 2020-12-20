5870b7d49db320ac92d0032e256ad6e2d79c909d
ezl-13 <ericzlou@gmail.com>
2020-12-19 23:26:43 -0500

    initialize firebase

---
+++ src/Firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAUrHjLAsNYF7rlJSdWfp5illkXkQH37xk",
+  authDomain: "bridgebuddies-b2de6.firebaseapp.com",
+  projectId: "bridgebuddies-b2de6",
+  storageBucket: "bridgebuddies-b2de6.appspot.com",
+  messagingSenderId: "879848527626",
+  appId: "1:879848527626:web:eea198a926b1424eda4cf3",
+  measurementId: "G-5XK225RR1K"
+};
+const Firebase = firebase.initializeApp(firebaseConfig);
+export default Firebase;
