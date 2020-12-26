812b64f4365fc9f719216c23626083ef201cfcf5
ezl-13 <ericzlou@gmail.com>
2020-12-26 13:33:01 -0500

    cursive no more!!

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
 
 import ComingSoonScreen from "./screens/ComingSoonScreen"
-import GameScreen from "./screens/GameScreen"
-import HomeScreen from "./screens/HomeScreen"
-import StoreScreen from "./screens/StoreScreen"
 
 
 function App() {
-  const COMING_SOON = true;
   return (
-    COMING_SOON ? <ComingSoonScreen/> : (
-    <Router>
-      <Switch>
-        <Route exact path="/"> <HomeScreen/> </Route>
-        <Route path="/store"> <StoreScreen/> </Route>
-        <Route path="/game"> <GameScreen/> </Route>
-        <Redirect to="/"/>
-      </Switch>
-    </Router>)
+    <ComingSoonScreen/>
   );
 }
 
