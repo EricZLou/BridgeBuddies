37c3452074b9521b80493d12a22555da012b3af4
ezl-13 <ericzlou@gmail.com>
2020-12-27 00:06:25 -0500

    4 people in room!

--- src/screens/GameScreenOnline.js
+++ src/screens/GameScreenOnline.js
@@ -2,13 +2,42 @@ import React from 'react';
 import {connect} from 'react-redux'
 
 import GameScreen from './GameScreen'
+import LoadingScreen from './LoadingScreen'
 import {ALL_SEATS, SEATS} from '../constants/GameEngine'
 
 import '../css/Style.css';
 
 class GameScreenOnline extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ready: false,
+    };
+    this.cleanup = this.cleanup.bind(this);
+  }
+
+  cleanup() {
+    this.props.mySocket.emit("leave online game");
+  }
+
+  componentDidMount() {
+    // clean up in case user reloads page
+    window.addEventListener("beforeunload", this.cleanup);
+    this.props.mySocket.on("start game", (game_info) => {
+      console.log(game_info);
+      this.setState({ready: true});
+    });
+    this.props.mySocket.emit("online game request", this.props.first_name);
+  }
+
+  componentWillUnmount() {
+    this.props.mySocket.emit("leave online game");
+    window.removeEventListener("beforeunload", this.cleanup);
+  }
+
   render() {
     const me = ALL_SEATS[Math.floor(Math.random() * 4)];
+    if (!this.state.ready) return (<LoadingScreen text={"Waiting for 4 players..."}/>);
     return (
       <GameScreen
         me={me}
@@ -27,6 +56,7 @@ class GameScreenOnline extends React.Component {
 const mapStateToProps = (state, ownProps) => {
   return {
     first_name: state.userDetails.first_name,
+    mySocket: state.mySocket,
   }
 }
 export default connect(mapStateToProps)(GameScreenOnline);
--- src/server/SocketManager.js
+++ src/server/SocketManager.js
@@ -1,34 +1,88 @@
 const io = require("./index.js").io;
-let num_connections = 0;
-let table = [];
 
-let num_users_logged_in = 0;
+let NUM_USERS_LOGGED_IN = 0;
+let GAME_IDX = 1;
+
+class DefaultDict {
+  constructor(defaultInit) {
+    return new Proxy({}, {
+      get: (target, name) => name in target ?
+        target[name] :
+        (target[name] = typeof defaultInit === 'function' ?
+          new defaultInit().valueOf() :
+          defaultInit)
+    })
+  }
+}
+/*
+  ONLINE_GAME_ROOMS = {
+    room1: {
+      qrjCPRDdQ6vY8b9fAAAE: "eric",
+      pTDpdDyNCaa1l-U1AAAH: "tim",
+      ExGqzw5Z99uxxzTZAAAJ: "elizabeth",
+      1GsjcrvmvBIUmpImAAAE: "paul",
+    },
+    room2: {
+
+    },
+    ...
+  }
+}
+*/
+let ONLINE_GAME_ROOMS = new DefaultDict(Map);
+
+function prepareGameInfoForRoom(room) {
+  console.log(room);
+  return "HELLO";
+}
 
 module.exports = function(socket) {
-  // AUTOMATIC USER LOG IN BEHAVIOR
-  num_users_logged_in++;
-  io.emit('num users logged in', num_users_logged_in);
+  /************************************************************
+    NUM USERS LOGGED IN
+  ************************************************************/
 
+  // AUTOMATIC USER LOG IN BEHAVIOR
+  NUM_USERS_LOGGED_IN++;
+  io.emit('num users logged in', NUM_USERS_LOGGED_IN);
+
+  // AUTOMATIC USER LOG OUT BEHAVIOR
   socket.on('disconnect', () => {
-    num_users_logged_in--;
-    io.emit('num users logged in', num_users_logged_in);
+    NUM_USERS_LOGGED_IN--;
+    io.emit('num users logged in', NUM_USERS_LOGGED_IN);
   });
 
-  // // LOG IN
-  // socket.on('login', () => {
-  //   console.log(`${socket.id} signed in`);
-  //   socket.join('room 1');
-  //   table.push(socket.id);
-  //   num_connections++;
-  //   if (table.length === 4) {
-  //     io.to(table[0]).emit('show', true);
-  //   }
-  // });
-  //
-  // // CARD CLICKED
-  // socket.on('clicked', () => {
-  //   console.log(`${socket.id} clicked`);
-  //   io.to('room 1').emit('show', false);
-  //   io.to(table[(table.indexOf(socket.id)+1) % 4]).emit('show', true);
-  // });
+
+  /************************************************************
+    ONLINE GAME ROOM DISPATCH
+  ************************************************************/
+
+  // HANDLE ONLINE GAME REQUEST
+  socket.on('online game request', (first_name) => {
+    // join a room
+    const room = `room${GAME_IDX}`;
+    socket.join(room);
+    socket.room = room;
+    socket.first_name = first_name;
+    ONLINE_GAME_ROOMS[room].set(socket.id, socket.first_name);
+    console.log(`[JOIN] ${room} - ${socket.id}`);
+    // if room is full, send notice to entire room to start game
+    if (ONLINE_GAME_ROOMS[room].size === 4) {
+      const game_info = prepareGameInfoForRoom(room);
+      io.to(room).emit('start game', game_info);
+      console.log(`[START GAME] ${room}`);
+    }
+  });
+
+  // HANDLE LEAVING THE ONLINE GAME SCREEN
+  socket.on('leave online game', () => {
+    console.log(`[LEAVE] ${socket.room} - ${socket.id}`);
+    socket.leave(socket.room);
+    ONLINE_GAME_ROOMS[socket.room].delete(socket.id);
+  });
+
+
+  /************************************************************
+    IN-GAME HANDLERS
+  ************************************************************/
+
 }
