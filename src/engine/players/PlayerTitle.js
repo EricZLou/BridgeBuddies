2873d155d2db76164876542ccfc83df782b1b2ed
ezl-13 <ericzlou@gmail.com>
2020-12-27 17:03:49 -0500

    player title

---
+++ src/engine/players/PlayerTitle.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import '../../css/PlayerTitle.css'
+
+import klubby from '../../media/store/characters/klubby.png'
+
+export default class PlayerTitle extends React.Component {
+  render() {
+    return (
+      <div className={this.props.is_my_turn ? "player-title-on" : "player-title-off"}>
+        <div className="player-title-pic">
+          <img src={klubby} alt="Profile"/>
+        </div>
+        <div className="player-title-name">
+          {this.props.name}
+        </div>
+        <div className="player-title-seat">
+          {this.props.seat}
+        </div>
+      </div>
+    )
+  }
+};
