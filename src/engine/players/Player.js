9de0ac4f38cb4a2492773d19207639301214c497
ezl-13 <ericzlou@gmail.com>
2020-12-27 17:02:31 -0500

    player

---
+++ src/engine/players/Player.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import Hand from '../Hand'
+import PlayerTitle from './PlayerTitle'
+
+import '../../css/Player.css'
+
+export default class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.seat = this.props.seat;
+  }
+
+  componentDidUpdate() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <Hand
+          cards={this.props.cards}
+          seat={this.props.seat}
+          handleHandClick={this.props.handlePlayerClick}
+          visible={this.props.visible}
+          clickable={this.props.clickable}
+        />
+        <div className="player-title">
+          <PlayerTitle
+            seat={this.seat}
+            is_my_turn={this.props.is_my_turn}
+            name={this.props.name}
+          />
+        </div>
+      </div>
+    )
+  }
+};
