fa00b5619ecb1c0ec3fad5a579778dfa7c374fc4
ezl-13 <ericzlou@gmail.com>
2020-12-29 00:00:04 -0500

    change link to heroku

--- src/screens/ComingSoonScreen.js
+++ src/screens/ComingSoonScreen.js
@@ -11,7 +11,7 @@ export default function ComingSoonScreen() {
     <div className="App-container">
         <div className="cs-title">BRIDGE BUDDIES</div>
         <img src={logo} className="App-logo" alt="logo" />
-        <a href="https://bridge-buddies.vercel.app/"
+        <a href="https://bridge-buddies.herokuapp.com/"
            target="_blank"
            rel="noopener noreferrer">
           <button className="cs-button">Sneak Peek at the Work-In-Progress</button>
