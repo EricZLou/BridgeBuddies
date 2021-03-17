3d726ea4100771a435021f3dbc16ae8c64afa5a9
EricZLou <ericzlou@gmail.com>
2021-03-16 21:42:28 -0400

    open in same tab, warn about portrait mobile

--- src/screens/ComingSoonScreen.js
+++ src/screens/ComingSoonScreen.js
@@ -12,10 +12,13 @@ export default function ComingSoonScreen() {
         <div className="cs-title">BRIDGE BUDDIES</div>
         <img src={logo} className="App-logo" alt="logo" />
         <a href="https://bridge-buddies.herokuapp.com/"
-           target="_blank"
            rel="noopener noreferrer">
           <button className="cs-button">Try the demo!</button>
         </a>
+        <div className="cs-note">
+          <div>NOTE:</div>
+          <div>Works best in landscape mode on a computer or tablet.</div>
+        </div>
         <img src={cover} alt="Characters" className="cs-characters"/>
     </div>
   );
